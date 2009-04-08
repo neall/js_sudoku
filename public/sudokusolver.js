ab1040193b771fb161075b0e64ce1eefeaba0e7a
Neal Lindsay <neal.lindsay@gmail.com>
2009-04-08 15:29:34 -0400

    Board object initializes itself with the numbers in the table

--- public/sudokusolver.js
+++ public/sudokusolver.js
@@ -1,18 +1,48 @@
 $(document).ready(function(){
 
   function Board(table) {
+    var dumpObj = function(obj, indent) {
+      indent = indent || ''
+      if (typeof obj === 'object') {
+        var retstring = "{\n";
+        for (key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            retstring = retstring + indent + '  ' + key + ': ' + dumpObj(obj[key], indent + '  ') + "\n";
+          }
+        }
+        retstring = retstring + indent + "}";
+        return retstring;
+      } else if (obj === undefined) {
+        return "undefined";
+      } else if (typeof obj === 'number') {
+        return obj.toString();
+      } else if (typeof obj === 'function') {
+        return 'Function';
+      } else {
+        return "'" + obj.toString() + "'";
+      }
+    }
     var newBoard = this;
     newBoard.rows = [];
-    $('tr', table).each(function(tableRow){
+    $('tr', table).each(function(i, tableRow){
       var row = [];
-      $('td', tableRow).each(function(tableCell){
+      $('td', tableRow).each(function(j, tableCell){
+        var cell = {
+          val: parseInt($(tableCell).text(), 10) || undefined
+        }
+        row.push(cell);
       });
       newBoard.rows.push(row);
     });
+    newBoard.debug = function(){
+      $('body').append('<pre>' + dumpObj(newBoard) + '</pre>');
+    }
+    return newBoard;
   }
 
-  $('.board').each(function(boardtable){
+  $('.board').each(function(i, boardtable){
     var board = new Board(boardtable);
+    // board.debug();
   });
 
 });
